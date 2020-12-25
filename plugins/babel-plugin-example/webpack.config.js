120f7660b68b482f79aba640b439e593ad142dd9
mengmeng19 <mengmeng19@staff.weibo.com>
2020-12-25 16:56:36 +0800

    babel

--- plugins/babel-plugin-example/webpack.config.js
+++
@@ -1,7 +0,0 @@
-const path = require('path');
-console.log('sss');
-module.exports = {
-  resolve: {
-    modules: [path.resolve(__dirname, './source'), path.resolve(__dirname, './node_modules')]
-  }
-}
